29e190a6ac59d9bd26c5daf70f57d4fc477ccd4d
DavoDev <davidlarrosa58@gmail.com>
2024-02-15 12:42:00 +0100

    first part tutorial

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { checkImageURL } from '../../../../utils'
+import styles from './nearbyjobcard.style'
+
+const NearbyJobCard = ({job, handleNavigate}) => {
+  
+  return (
+    <TouchableOpacity 
+        style={styles.container} 
+        onPress={handleNavigate}>
+      <View>
+      <TouchableOpacity 
+          style={styles.logoContainer}>
+        <Image 
+            source={{uri: job.employer_logo}} 
+            resizeMode='contain' 
+            style={styles.logImage}/>
+      </TouchableOpacity>
+      <Text 
+          style={styles.companyName}
+          numberOfLines={1} >{job.employer_name}
+      </Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={2}>
+          {job?.job_title}
+        </Text>
+        <Text style={styles.jobType}>{job?.employment_type}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default NearbyJobCard
